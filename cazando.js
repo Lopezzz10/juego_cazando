2232df3010e4c8411e9a9d159f81a37b8f54d808
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-04-12 09:11:15 -0500

    Define variables y constantes de posición, comida en esquina inferior derecha

--- cazando.js
+++ cazando.js
@@ -9,14 +9,14 @@ const VELOCIDAD = 15;
 // Gato
 let gatoX = 0;
 let gatoY = 0;
-const ANCHOGATO = 50;
-const ALTURAGATO = 50;
+const ALTO_GATO = 50;
+const ANCHO_GATO = 50;
  
 // Comida
-let comidaX = 100;
-let comidaY = 100;
-const ANCHOCOMIDA = 30;
-const ALTURACOMIDA = 30;
+let comidaX = 0;
+let comidaY = 0;
+const ALTO_COMIDA = 30;
+const ANCHO_COMIDA = 30;
  
 function graficar(x, y, ancho, alto, color) {
     ctx.fillStyle = color;
@@ -24,16 +24,18 @@ function graficar(x, y, ancho, alto, color) {
 };
  
 function graficarGato() {
-    graficar(gatoX, gatoY, ANCHOGATO, ALTURAGATO, "#000000");
+    graficar(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "#000000");
 };
  
 function graficarComida() {
-    graficar(comidaX, comidaY, ANCHOCOMIDA, ALTURACOMIDA, "#ff0000");
+    graficar(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "#ff0000");
 };
 
 function iniciarJuego(){
-    gatoX = (canvas.width / 2) - (ANCHOGATO / 2);
-    gatoY = (canvas.height / 2) - (ALTURAGATO / 2);
+    gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
+    gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
+    comidaX = canvas.width - ANCHO_COMIDA;
+    comidaY = canvas.height - ALTO_COMIDA;
     graficarGato();
     graficarComida();
 }
